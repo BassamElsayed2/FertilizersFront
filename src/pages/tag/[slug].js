8b35bdaee44f63874112db2b938780aa2c88e4b3
Bassam-Elsaied <bassame993@gmail.com>
2025-07-01 17:46:27 +0300

    fix [slug]page

--- src/pages/tag/[slug].js
+++ src/pages/tag/[slug].js
@@ -1,12 +1,13 @@
 import { useRouter } from "next/router";
 import { getAllPosts } from "../../../lib/api";
-import GalleryOne from "../common/gallery/GalleryOne";
+
 import FooterOne from "../../common/elements/footer/FooterOne";
 import HeaderOne from "../../common/elements/header/HeaderOne";
 import PostLayoutTwo from "../../common/components/post/layout/PostLayoutTwo";
 import SidebarOne from "../../common/components/sidebar/SidebarOne";
 import BreadcrumbOne from "../../common/elements/breadcrumb/breadcrumbOne";
 import { slugify } from "../../common/utils";
+import GalleryOne from "../../common/gallery/GalleryOne";
 
 const TagsArchive = ({ tagsData, allPosts }) => {
   const router = useRouter();
